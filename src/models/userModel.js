e43bb16fcc30b4aac8ffce78d3f546ce8c33e920
Dharmendra Yadav <kumardharmendra81202@gmail.com>
2022-06-06 13:22:26 +0530

    Data-Base-Assignment

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+const bookSchema = new mongoose.Schema( {
+    bookName: String,
+    authorName: String,
+    category: {
         type: String,
-        unique: true,
-        required: true
+        // unique: true,
+        // required: true
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
+    
+   
+    year: Number,
     // isIndian: Boolean,
     // parentsInfo: {
     //     motherName: String,
@@ -23,7 +20,7 @@ const userSchema = new mongoose.Schema( {
     // cars: [ String  ]
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('Book', bookSchema) //users
 
 
 
