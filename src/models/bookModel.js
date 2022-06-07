5b00796b1bc963406bbf448f48c5994b3c53d4f5
Dharmendra Yadav <kumardharmendra81202@gmail.com>
2022-06-07 23:52:19 +0530

    Assignment/mongo-session2-part2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,15 @@
 const mongoose = require('mongoose');
 
+/*----------------Create BookSchema------------------------*/
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    sales: {type: Number, default: 10}
+     name:String,
+     author_id:{
+         type:Number,
+         required:true
+     },
+     price:Number,
+     ratings:Number
 }, { timestamps: true });
 
+module.exports = mongoose.model('Book', bookSchema)
 
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
