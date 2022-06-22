ebf6c4f4c3637524ecf654ba15ae8ed506bacc92
Avijit07 <happyavijit1998@gmail.com>
2022-06-22 21:32:22 +0530

    phase1 part2

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,21 +1,8 @@
 const blogModels = require("../models/blogModels");
-const mongoose=require('mongoose')
-const authorModel=require('../models/authorModels')
+const mongoose = require('mongoose')
+const authorModel = require('../models/authorModels')
 
-/***************************VALIDATION****************************************/
-// const isValid = function (x) {
-//     if (typeof x === "undefined" || x === null) return false;
-//     if (typeof x === "string" && x.trim().length === 0) return false;
-//     return true;
-//   };
 
-//   const isValidBody = function (x) {
-//     return Object.keys(x).length > 0;
-//   };
-
-//   const isValidObjectId=function(x){
-//      return mongoose.Types.ObjectId.isValid(x)
-//   }
 
 const creatBlog = async function (req, res) {
   try {
@@ -39,32 +26,68 @@ const creatBlog = async function (req, res) {
     // }
     // const authorId2=await authorModel.findOne({_id:authorId})
     // console.log(authorId2)      
-      
+
     // if(tags){
     //     if(Array.isArray(tags)){
     //         newblogsData[`tags`]=[...tags]
     //     }
 
     // }
-      if(data.isPublished===false){
-        let blogData = await blogModels.create(data);
-        blogData.publishedAt=null;
-        res.status(201).send({ status: true, msg: blogData });
-      }
+    if (data.isPublished === false) {
+      let blogData = await blogModels.create(data);
+      blogData.publishedAt = null;
+      res.status(201).send({ status: true, msg: blogData });
+    }
 
-      if(data.isPublished===true){
-        let blogData = await blogModels.create(data);
-        blogData.publishedAt=new Date();
-        res.status(201).send({ status: true, msg: blogData });
-      }
-
-
-    
-    
+    if (data.isPublished === true) {
+      let blogData = await blogModels.create(data);
+      blogData.publishedAt = new Date();
+      res.status(201).send({ status: true, msg: blogData });
+    }
   } catch (error) {
     res.status(500).send({ status: false, msg: error.message });
   }
 };
 
-/******************************PUBLIC**********************************/
+const updateBlogs = async function (req, res) {
+
+  try {
+    let blogId = req.params.blogId
+    let body = req.body;
+
+    let updatedBlogData = await blogModels.findByIdAndUpdate(blogId, body, { new: true });
+
+    if (!updatedBlogData) {
+      return res.status(404).send({ status: false, msg: "Data not found" })
+    } else if (updatedBlogData.isPublished == true) {
+
+      updatedBlogData.publishedAt = new Date();
+      res.status(200).send({ status: true, data: updatedBlogData })
+    }
+  } catch (error) {
+    res.status(500).send({ status: false, msg: "Server error" });
+  }
+}
+
+const deleteBlog = async function (req, res) {
+  try {
+    let blogId = req.params.blogId
+    let updatedBlog = await blogModels.findOneAndUpdate({ _id: blogId }, { $set: { isDeleted: true } }, { new: true })
+
+    if (!updatedBlog) {
+      return res.status(404).send({ status: false, msg: "Blog is not available" })
+    } else if (updatedBlog.isDeleted == true) {
+      updatedBlog.deletedAt = new Date();
+      res.status(200).send({ status: true, data:" "})
+    }
+
+    res.status(200).send({ data: updatedBlog })
+  } catch (error) {
+    res.status(500).send({ status: false, msg: "Server error" });
+  }
+}
+
+
 module.exports.creatBlog = creatBlog;
+module.exports.updateBlogs = updateBlogs
+module.exports.deleteBlog = deleteBlog
--- router/route.js
+++ router/route.js
@@ -3,8 +3,13 @@ const router=express.Router()
 const authorController=require('../controller/authorController')
 const blogController=require('../controller/blogController')
 
-/***********************POST API*******************************/
+
 router.post("/authors",authorController.creatAuthor)
+
 router.post("/blogs",blogController.creatBlog)
+
+router.put("/blogs/:blogId", blogController.updateBlogs)
+
+router.delete("/blogs/:blogId", blogController.deleteBlog)
 
 module.exports=router
